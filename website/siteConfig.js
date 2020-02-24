4b7718a518dc2ea83a137b24070e8dae1bdb2e18
Denis Granha <denisgranha@gmail.com>
2020-02-24 11:58:35 +0100

    change names

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,16 +21,16 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Gnosis Developer Portal Conditional Tokens', // Title for your website.
-  tagline: 'Create conditional tokens, a new asset class with richer informational capabilities that makes the outcome of any future event tradable.',
-  url: 'https://docs.gnosis.io', // Your website URL
+  title: 'Gnosis Developer Portal Dfusion', // Title for your website.
+  tagline: 'Dfusion',
+  url: 'https://dfusion-docs.gnosis.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'gnosis-docs',
+  projectName: 'dfusion-docs',
   organizationName: 'gnosis',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
