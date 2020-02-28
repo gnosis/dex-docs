49ae4d5f9d3474429c0ced35cbbd6a0d1043653a
Denis Granha <denisgranha@gmail.com>
2020-02-28 17:17:00 +0100

    baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Gnosis Developer Portal dƒusion ', // Title for your website.
   tagline: 'Dfusion',
   url: 'https://dfusion-docs.gnosis.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/dfusion/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
